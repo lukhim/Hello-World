9d1c378994170ce422b1027726ac3bef29b94dc9
lookman010602 <45191695+lookman010602@users.noreply.github.com>
2019-08-02 08:50:44 +0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+name = 'Lukmanul Hakim'
+region = 'Indonesia'
+what_i_wanna_be = 'Great Programmer'
+print(f'''Hello World my name is {name}, and i'm from {region}, i hope i can become {what_i_wanna_be}''')
